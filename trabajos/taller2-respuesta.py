a0a057660745a319533f91897a73eeacb35803ee
Sergio_Palacios_2 <sergid26pal@gmail.com>
2025-03-08 22:08:20 -0500

    Clase ejercios del taller2-respuesta.py completa

--- trabajos/taller2-respuesta.py
+++ trabajos/taller2-respuesta.py
@@ -14,27 +14,33 @@ def pendiente(x0,x1,y0,y1):
 def recta(x0,y0,m):
     return y0-(m*x0)
 
-def toDBFS(A):
-    return 10*np.log10(A/32767)
+def DBtoBYTE(db):
+    return 10**(db/10)
 
-def ondaSeno(Fm,w,T,a=1,phi=0):
+def ondaSeno(w,a,phi=0):
+    Fm = 44100
+    T = 3
     s = []
-    for t in range(Fm*T):
+    for t in range(int(Fm*T)):
         aux = (a*np.sin((2*np.pi/Fm)*w*t))+phi
         s.append(aux)   
-    return np.array(s)
+    return s
 
-def ondaCuadrada(Fm,w,T,a=1,phi=0):
+def ondaCuadrada(w,a,phi=0):
+    Fm = 44100
+    T = 3
     s = []
-    for t in range(Fm*T):
+    for t in range(int(Fm*T)):
         aux = (np.sin((2*np.pi/Fm)*w*t))+phi
         if(aux < 0):
             s.append(-a)   
         else:
             s.append(a)
-    return np.array(s)
+    return s
 
-def ondaTriangulo(Fm,w,T,a=1,phi=0):
+def ondaTriangulo(w,a,phi=0):
+    Fm = 44100
+    T = 3
     periodo = int(Fm/w)
     frecuencia = int(T*w)
 
@@ -56,9 +62,11 @@ def ondaTriangulo(Fm,w,T,a=1,phi=0):
             else:
                 s.append((m3*x)+b3)
     
-    return np.array([i*a for i in s])
+    return [i*a for i in s]
 
-def ondaSierra(Fm,w,T,a=1,phi=0):
+def ondaSierra(w,a,phi=0):
+    Fm = 44100
+    T = 3
     periodo = int(Fm/w)
     frecuencia = int(T*w)
 
@@ -76,24 +84,198 @@ def ondaSierra(Fm,w,T,a=1,phi=0):
             else:
                 s.append((m2*x)+b2)
     
-    return np.array([i*a for i in s])
+    return [i*a for i in s]
 
 class ejercicios:
-    def __init__(self,onda1,freq1,onda2,freq2,amplitud,periodo):
+    def __init__(self,onda1,freq1,onda2,freq2,amplitud):
         self.onda1 = onda1
-        self.freq1 = freq1
         self.onda2 = onda2
+        self.freq1 = freq1
         self.freq2 = freq2
         self.a = amplitud
-        self.z = periodo/4
-        self.w = periodo/8
-        self.tiempo = 3
-    
+        self.T1 = int(44100/freq1)
+        self.T2 = int(44100/freq2)
+
     def punto1(self):
-        pass
+        x = self.onda1(self.freq1,self.a)
+        y = self.onda2(self.freq2,self.a)
 
-onda = ondaSierra(8000,10,3,a=2)
-tiempo = [t for t in range(len(onda))]
+        rta = (x + y)
+        tiempo = np.linspace(0,3,len(rta))
 
-plt.plot(tiempo,onda)
-plt.show()
+        plt.plot(tiempo,rta)
+        plt.show()
+    
+    def punto2(self):
+        x = self.onda1(self.freq1,self.a)
+        y = self.onda2(self.freq2,self.a)
+
+        rta = (x + y)
+        
+        tiempo = np.linspace(0,3,len(rta))
+        plt.plot(tiempo,[-i for i in rta])
+        plt.show()
+    
+    def punto3(self):
+        x = self.onda1(self.freq1,self.a)
+        x = [x[n] for n in range(len(x)) if(n%2==0)]
+
+        y = self.onda2(self.freq2,self.a)
+        y = [y[n] for n in range(len(y)) if(n%2==0)]
+
+        rta = (x + y)
+        
+        tiempo = np.linspace(0,3,len(rta))
+        plt.plot(tiempo,rta)
+        plt.show()
+
+    def punto4(self):
+        x = self.onda1(self.freq1,self.a)
+        x = [x[n] for n in range(len(x)) if(n%3==0)]
+
+        y = self.onda2(self.freq2,self.a)
+        y = [y[n] for n in range(len(y)) if(n%3==0)]
+
+        rta = (x + y)
+        
+        tiempo = np.linspace(0,3,len(rta))
+        plt.plot(tiempo,rta)
+        plt.show()
+
+    def punto5(self):
+        x = self.onda1(self.freq1,self.a)
+        y = self.onda2(self.freq2,self.a)
+
+        rta = (x + y)
+        
+        tiempo = np.linspace(0,3,len(rta))
+        plt.plot(tiempo,[i/2 for i in rta])
+        plt.show()
+    
+    def punto6(self):
+        x = self.onda1(self.freq1,self.a)
+        y = self.onda2(self.freq2,self.a)
+        
+        size = len(x)
+        if(size > len(y)):
+            size = len(y)
+
+        rta = [(x[n]+y[n]) for n in range(size)]
+
+        tiempo = np.linspace(0,3,len(rta))
+        plt.plot(tiempo,rta)
+        plt.show()
+
+    def punto7(self):
+        x = self.onda1(self.freq1,self.a)
+        
+        rta = (x + [x[n] for n in range(len(x)) if(n%2==0)] + [x[n] for n in range(len(x)) if(n%3==0)])
+
+        tiempo = np.linspace(0,3,len(rta))
+        plt.plot(tiempo,rta)
+        plt.show()
+
+    def punto8(self):
+        x = self.onda1(self.freq1,self.a)
+        y = self.onda2(self.freq2,self.a)
+        
+        rta = ([0.5*n for n in x] + [0.5*n for n in y])
+
+        tiempo = np.linspace(0,3,len(rta))
+        plt.plot(tiempo,rta)
+        plt.show()
+    
+    def punto9(self):
+        x = self.onda1(self.freq1,self.a)
+        y = self.onda2(self.freq2,self.a)
+        z1 = int(self.T1/4)
+        z2 = int(self.T2/4)
+        
+        rta = (x[z1:] + y[z2:])
+
+        tiempo = np.linspace(0,3,len(rta))
+        plt.plot(tiempo,rta)
+        plt.show()
+
+    def punto10(self):
+        x = self.onda1(self.freq1,self.a)
+        y = self.onda2(self.freq2,self.a)
+        
+        z1 = int(self.T2/4)
+        x = x[z1:]
+
+        size = len(x)
+        if(size > len(y)):
+            size = len(y)
+
+        rta = [y[n]+x[n] for n in range(size)]
+
+        tiempo = np.linspace(0,3,len(rta))
+        plt.plot(tiempo,rta)
+        plt.show()
+    
+    def punto11(self):
+        x = self.onda1(self.freq1,self.a)
+        y = self.onda2(self.freq2,self.a)
+        
+        x1 = [n*0.5 for n in x]
+        y = [y[n*2]*0.3 for n in range(int(len(y)/2))]
+        x2 = [x[n*3]*0.2 for n in range(int(len(x)/3))]
+
+        size = len(x1)
+        if(size > len(y)):
+            size = len(y)
+        if(size > len(x2)):
+            size = len(x2)
+
+        rta = [x1[n]+y[n]+x2[n] for n in range(size)]
+
+        tiempo = np.linspace(0,3,len(rta))
+        plt.plot(tiempo,rta)
+        plt.show()
+
+    def punto12(self):
+        x = self.onda1(self.freq1,self.a)
+        y = self.onda2(self.freq2,self.a)
+        w = int(self.T1/8)
+        z = int(self.T2/4)
+        
+        x1 = [n*0.5 for n in x[w:]]
+        y = [y[2*n-z]*0.3 for n in range(int(len(y[z:])/2))]
+        x2 = [x[3*n]*0.2 for n in range(int(len(x)/3))]
+        
+        size = len(x1)
+        if(size > len(y)):
+            size = len(y)
+        if(size > len(x2)):
+            size = len(x2)
+
+        rta = [(x1[n]+y[n]+x2[n]) for n in range(size)]
+
+        tiempo = np.linspace(0,3,len(rta))
+        plt.plot(tiempo,rta)
+        plt.show()
+
+# RESPUESTAS #
+
+amplitud = DBtoBYTE(-6)
+
+#test = ondaSeno(400,1,amplitud)
+#plt.plot([i for i in range(len(test))],[-i for i in test])
+#plt.show()
+
+""" 1. X[n] = señal seno a 500Hz, y[n] = señal diente de sierra a 750Hz """
+
+primero = ejercicios(ondaSeno,500,ondaSierra,750,amplitud)
+primero.punto1()
+primero.punto2()
+primero.punto3()
+primero.punto4()
+primero.punto5()
+primero.punto6()
+primero.punto7()
+primero.punto8()
+primero.punto9()
+primero.punto10()
+primero.punto11()
+primero.punto12()
